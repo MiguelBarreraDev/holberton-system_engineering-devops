ecbc7ce82a43f1777d44de31e489007476a9bc34
MiguelBarreraDev <barreram987@gmail.com>
2022-04-15 16:16:50 -0500

    Add: provider

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 # Using Puppet, install puppet-lint
 package { 'puppet-lint':
-  ensure => '2.5.0',
-  name   => 'puppet-lint',
+  ensure   => '2.5.0',
+  name     => 'puppet-lint',
+  provider => 'gem',
 }
