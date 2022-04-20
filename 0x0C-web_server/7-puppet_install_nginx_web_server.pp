f57bd84705395b3883e2bce171105c7afdca763b
MiguelBarreraDev <barreram987@gmail.com>
2022-04-20 01:10:40 -0500

    Add puppet file

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,12 @@
+# nstall Nginx web server (All previous puppet)
+
+exec {'install Nginx and redirect_me':
+  provider => shell,
+  command  => 'sudo apt-get -y update ;\
+                sudo apt-get -y install nginx ;\
+                sudo chown -R ubuntu /var/www ;\
+                echo "Hello World" | sudo tee /var/www/html/index.nginx-debian.html ;\
+                sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/MiguelBarreraDev permanent;/" \
+                    /etc/nginx/sites-available/default ;\
+                sudo service nginx start',
+}I
