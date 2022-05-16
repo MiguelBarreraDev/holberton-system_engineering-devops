783b2bcd22a2c836e4d19f6ad23bfd58bfbbc2f8
MiguelBarreraDev <barreram987@gmail.com>
2022-05-16 03:03:43 -0500

    Starting project

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Gather data from an API
+Write a Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+import requests
+from sys import argv
+
+# Data
+ID = argv[1]
+url_api = "https://jsonplaceholder.typicode.com"
+
+# Endpoints
+ep_user = "{}/users/{}".format(url_api, str(ID))
+ep_todos = "{}/todos".format(ep_user)
+ep_todos_completed = "{}?completed=true".format(ep_todos)
+
+# Requests
+user = requests.get(ep_user)
+todos = requests.get(ep_todos)
+todos_completed = requests.get(ep_todos_completed)
+
+# Format
+user = user.json()
+todos = todos.json()
+completed = todos_completed.json()
+
+# Output
+print("Employee {} is done with tasks({}/{}):".format(
+    user.get("name"), len(completed), len(todos)
+))
+for task in completed:
+    print("\t {}".format(task.get("title")))
