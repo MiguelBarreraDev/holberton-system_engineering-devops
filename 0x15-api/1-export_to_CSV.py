fe1de0b019333a951d1a88ef20e5ac927195b1c1
MiguelBarreraDev <barreram987@gmail.com>
2022-05-16 16:15:22 -0500

    Add: Requeriment, Your code should not be executed when imported

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python3
 """Export to CSV"""
-import csv
-import json
-from sys import argv
-import requests
+if __name__ == "__main__": 
+    import csv
+    import json
+    from sys import argv
+    import requests
 
-# Data
-ID = argv[1]
-url_api = "https://jsonplaceholder.typicode.com"
+    # Data
+    ID = argv[1]
+    url_api = "https://jsonplaceholder.typicode.com"
 
-# Endpoints
-ep_user = "{}/users/{}".format(url_api, str(ID))
-ep_todos = "{}/todos".format(ep_user)
+    # Endpoints
+    ep_user = "{}/users/{}".format(url_api, str(ID))
+    ep_todos = "{}/todos".format(ep_user)
 
-# Requests
-user = requests.get(ep_user)
-todos = requests.get(ep_todos)
+    # Requests
+    user = requests.get(ep_user)
+    todos = requests.get(ep_todos)
 
-# Format
-user = user.json()
-todos = todos.json()
+    # Format
+    user = user.json()
+    todos = todos.json()
 
-# Export to .csv file
-with open(ID + ".csv", "w", encoding="utf8") as file:
-    writer = csv.writer(file, delimiter=",", quotechar="'")
-    for task in todos:
-        writer.writerow([
-            json.dumps(ID),
-            json.dumps(user.get("username")),
-            json.dumps(str(task.get("completed"))),
-            json.dumps(task.get("title"))
-        ])
+    # Export to .csv file
+    with open(ID + ".csv", "w", encoding="utf8") as file:
+        writer = csv.writer(file, delimiter=",", quotechar="'")
+        for task in todos:
+            writer.writerow([
+                json.dumps(ID),
+                json.dumps(user.get("username")),
+                json.dumps(str(task.get("completed"))),
+                json.dumps(task.get("title"))
+            ])
