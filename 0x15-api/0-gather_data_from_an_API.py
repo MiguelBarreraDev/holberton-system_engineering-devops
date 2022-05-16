6a68682881c128188c1641ca83e0defb72a59446
MiguelBarreraDev <barreram987@gmail.com>
2022-05-16 16:11:47 -0500

    Documentation of module

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""
-Gather data from an API
-Write a Python script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress.
-"""
+"""Gather data from an API"""
 if __name__ == "__main__":
     import requests
     from sys import argv
