d41fc256b1f51822edc9889e9973797d34219ef2
MiguelBarreraDev <barreram987@gmail.com>
2022-05-16 03:43:37 -0500

    Add: Script that export data in csv format

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,1 +1,37 @@
 #!/usr/bin/python3
+"""
+Export to CSV
+Using what you did in the task #0, extend your Python script to
+export data in the CSV format.
+"""
+import csv
+import json
+from sys import argv
+import requests
+
+# Data
+ID = argv[1]
+url_api = "https://jsonplaceholder.typicode.com"
+
+# Endpoints
+ep_user = "{}/users/{}".format(url_api, str(ID))
+ep_todos = "{}/todos".format(ep_user)
+
+# Requests
+user = requests.get(ep_user)
+todos = requests.get(ep_todos)
+
+# Format
+user = user.json()
+todos = todos.json()
+
+# Export to .csv file
+with open(ID + ".csv", "w", encoding="utf8") as file:
+    writer = csv.writer(file, delimiter=",", quotechar="'")
+    for task in todos:
+        writer.writerow([
+            json.dumps(ID),
+            json.dumps(user.get("username")),
+            json.dumps(str(task.get("completed"))),
+            json.dumps(task.get("title"))
+        ])
