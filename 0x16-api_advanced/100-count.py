cbf298792a25d840416e7ff7ab92dfc3fc24342c
MiguelBarreraDev <barreram987@gmail.com>
2022-05-31 19:22:24 -0500

    Add: Parses the title of all hot articles, and prints a sorted count of given keywords

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -23,6 +23,30 @@ def recurse(subreddit, hot_list=[], after=""):
     return None
 
 
+def my_sorted(my_dict):
+    """definition"""
+    result = []
+    items_list = my_dict.items()
+    items_list = sorted(items_list, key=lambda item: item[1])
+    items_list = list(reversed(items_list))
+    i = 0
+    j = 0
+    while len(result) < len(items_list):
+        band = j <= len(items_list) - 2
+        if band and items_list[j][1] == items_list[j + 1][1]:
+            while band and items_list[j][1] == items_list[j + 1][1]:
+                j += 1
+            sub = sorted(items_list[i:j + 1], key=lambda item: item[0])
+            result += sub
+            i = j + 1
+            j += 1
+            continue
+        result.append(items_list[j])
+        i += 1
+        j += 1
+    return dict(result)
+
+
 def count_words(subreddit, word_list):
     """
     parses the title of all hot articles, and prints a sorted count of
@@ -39,9 +63,8 @@ def count_words(subreddit, word_list):
 
     for word in word_list:
         dict_word[word] += titles.count(word)
+    sort_dict = my_sorted(dict_word)
 
-    sort_dict = dict(sorted(dict_word.items(), key=lambda item: item[1]))
-
-    for key, value in reversed(sort_dict.items()):
+    for key, value in sort_dict.items():
         if value > 0:
             print(f"{key}: {value}")
