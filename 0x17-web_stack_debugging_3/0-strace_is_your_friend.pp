d4e949841466451ac0dd201092782e8eee66be40
MiguelBarreraDev <barreram987@gmail.com>
2022-06-01 14:26:58 -0500

    Add: path of the command

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,5 @@
 # fix to why Apache is returning a 500 error
--> exec { 'typo':
-  command => 'sed -i "s/phpp/php" /var/www/html/wp-settings.php'
-}
--> exec { 'Reload Service':
-  command => 'service apache2 reload'
+-> exec { 'fix typo':
+  command => 'sed -i "s/phpp/php" /var/www/html/wp-settings.php',
+  path    => '/usr/bin:/bin'
 }
