bae7eb0f3542b0a10fb6be334c690292cc44b764
MiguelBarreraDev <barreram987@gmail.com>
2022-06-01 14:29:13 -0500

    Add: path of the command

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # fix to why Apache is returning a 500 error
--> exec { 'fix typo':
+exec { 'fix typo':
   command => 'sed -i "s/phpp/php" /var/www/html/wp-settings.php',
   path    => '/usr/bin:/bin'
 }
